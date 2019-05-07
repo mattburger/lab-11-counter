86ada157fa9278d2d4489489539f8c9b85b64fb1
Matt Burger <matt.burger1@outlook.com>
2019-05-07 09:31:47 -0700

    counter ready for test1

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,9 +1,56 @@
 import React from 'react';
 import { ReactComponent } from '*.svg';
 
-class App() extends ReactComponent {
-  return (
+const Header = () => {
+  return(
+    <header>
+      <h1>Lab-11-React-Counter</h1>
+    </header>
   );
+};
+
+class Content extends ReactComponent {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  handleClickMinus = e => {
+    e.preventDefault();
+    let counter = this.state.counter--;
+
+    this.setState({counter});
+  }
+  handleClickPlus = el => {
+    el.preventDefault();
+    let counter = this.state.counter++;
+
+    this.setState({counter});
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleClickMinus}>-</button>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this.handleClickPlus}>+</button>
+      </div>
+    );
+  }
+}
+
+class App extends ReactComponent {
+  render(){
+    return (
+      <React.Fragment>
+        <Header />
+        <Content />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
